048c1e10da4432621e30cb96455f238832ab631d
clarencetinator7 <clarencejoseph.dimafelix@gmail.com>
2023-01-07 13:05:41 +0800

    add: dummy data for tasks

--- src/components/Layout/Main Panel/TaskPanel.jsx
+++ src/components/Layout/Main Panel/TaskPanel.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 import TaskForm from "../../Tasks/TaskForm";
+import TaskList from "../../Tasks/TaskList";
 
 import style from './TaskPanel.module.css';
+
+const INITIAL_TASKS = [
+  {
+    id: 1,
+    taskName: "Buy groceries",
+    due: "2023-01-06"
+  },
+  {
+    id: 2,
+    taskName: "Finish project proposal",
+    due: "2023-01-07"
+  },
+  {
+    id: 3,
+    taskName: "Attend yoga class",
+    due: "2023-01-08"
+  },
+  {
+    id: 4,
+    taskName: "Call dentist for appointment",
+    due: "2023-01-09"
+  },
+  {
+    id: 5,
+    taskName: "Submit expense report",
+    due: "2023-01-10"
+  }
+]
 
 const TaskPanel = () => {
   return (
@@ -11,6 +40,7 @@ const TaskPanel = () => {
           <span className="content-title">TODAY:</span>
         </div>
         <TaskForm />
+        <TaskList taskList={INITIAL_TASKS}/>
       </div>
     </main>
   );
--- src/components/Tasks/TaskList.jsx
+++ src/components/Tasks/TaskList.jsx
@@ -1,12 +1,28 @@
-import react from 'react';
+import React from 'react';
 
 import style from './TaskList.module.css';
 
-const TaskList = () => {
+const TaskList = props => {
+
+  const taskItems = props.taskList.map((task) => {
+    return (
+      <li key={task.id} className="task-item">
+        <div className="task-item__wrapper">
+          <input type="checkbox" name={task.id} id={task.id} />
+          <label htmlFor={task.id}>{task.taskName}</label>
+        </div>
+        <div className="task-item__wrapper">
+          <span>{task.due}</span>
+        </div>
+      </li>
+    );
+  });  
 
   return (
-    <div>
-      
+    <div className='task-list__container'>
+      <ul className='task-list'>
+        {taskItems}
+      </ul>
     </div>
   );  
 
