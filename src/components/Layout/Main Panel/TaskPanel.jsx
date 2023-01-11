2f6e6200327ee4b80dff882f0bfd37cc4b880694
clarencetinator7 <clarencejoseph.dimafelix@gmail.com>
2023-01-11 15:04:49 +0800

    Task adds on the current active list

--- src/components/Layout/Main Panel/TaskPanel.jsx
+++ src/components/Layout/Main Panel/TaskPanel.jsx
@@ -12,22 +12,30 @@ const TaskPanel = (props) => {
   const taskCtx = useContext(TaskContext);
   let filteredTasks = taskCtx.tasks;
 
+  let isList = false;
+  let listName = '';
+
   let finishedTasks = taskCtx.tasks.filter((item) => {
     return item.isDone;
   });
 
 
   if(props.displayed === 'TODAY') {
+    listName = '🌞 Today'
     filteredTasks = taskCtx.getSortedTasks().filter(item => {
       return moment(item.due).isSame(moment(), 'day') || item.due === null;
     });
   } else if(props.displayed === 'INBOX') {
+    listName = '📥 Inbox'
     filteredTasks = taskCtx.getSortedTasks();
   } else if(props.displayed === 'NEXTWEEK') {
+    listName = '📅 Next Week'
     filteredTasks = taskCtx.getSortedTasks().filter(item => {
       return moment(item.due).isAfter(moment(), 'week')
     });
   } else {
+    isList = true;
+    listName = props.displayed
     filteredTasks = taskCtx.getSortedTasks().filter(item => {
       return item.list === props.displayed;
     })
@@ -40,9 +48,9 @@ const TaskPanel = (props) => {
     <main className={style["main-content"]}>
       <div className={style["content-container"]}>
         <div className={style["main-content__header"]}>
-          <span className="content-title">{`${props.displayed}:`}</span>
+          <span className="content-title">{`${isList ? '#️' : ''} ${listName}:`}</span>
         </div>
-        <TaskForm/>
+        <TaskForm isList={isList} listName={listName}/>
         <TaskList displayedTasks={filteredTasks}/>
         <FinishedTasksList finishedTasks={finishedTasks} />
       </div>
