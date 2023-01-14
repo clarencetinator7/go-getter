e24555b0422409676c740d951ba95b8d8bad08a7
clarencetinator7 <clarencejoseph.dimafelix@gmail.com>
2023-01-14 13:10:24 +0800

    feat: responsive sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,18 @@ import TaskProvider from './components/Layout/Context/TaskProvider';
 import TaskPanel from './components/Layout/Main Panel/TaskPanel';
 import Sidebar from './components/Layout/Sidebar/Sidebar';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+const faIcons = {
+  bars: <FontAwesomeIcon icon={faBars} className="bars-icon" />,
+};
+
+
 function App() {
   
   const [currDisplayed, setDisplayed] = useState('INBOX');
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   const setDisplayedHandler = (display) => {
     setDisplayed(display);
@@ -15,7 +24,17 @@ function App() {
   return (
     <TaskProvider>
       <div className="parent-container">
-        <Sidebar setDisplay={setDisplayedHandler} displayed={currDisplayed}/>
+        <div className="top-bar">
+          <div className="top-bar__left">
+            <span className="top-bar__left__icon" onClick={() => {setSidebarOpen(!sidebarOpen)}}>
+              {faIcons.bars}
+            </span>
+            <span className="top-bar__left__title">
+              Go-Getter
+            </span>
+          </div>
+        </div>
+        <Sidebar setDisplay={setDisplayedHandler} displayed={currDisplayed} sidebarOpen={sidebarOpen} setSidebar={setSidebarOpen}/>
         <TaskPanel displayed={currDisplayed} />
       </div>
     </TaskProvider>
